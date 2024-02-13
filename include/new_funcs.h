469c3ad03564c0505540279788e796c86db91d54
Aegis-ID <pierre.nguyen@epitech.eu>
2024-02-13 11:28:40 +0100

    {add} add 3 new funcs

--- include/new_funcs.h
+++ include/new_funcs.h
@@ -8,14 +8,11 @@
 #ifndef __MY_LIB__
     #define __MY_LIB__
 
+int check_file_extension(const char *str, const char *ext);
+int count_char_in_str(char *str, char c);
 long get_size(const char *filename);
 int my_readfile(char *filepath);
-int my_strcmp(char const *s1, char const *s2);
-
-//map_analyser.c
-int map_row(char **map);
-char *file_content(char *filepath);
-char **buffer_to_map(char *buffer);
-void free_map(char **map);
+int my_strlen_delim(char *str, int start, char delim);
+char **str_to_word_array(char *str, char delimiter);
 
 #endif /* !__MY_LIB__ */
