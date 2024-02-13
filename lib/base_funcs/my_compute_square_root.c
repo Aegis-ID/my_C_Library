48e1778418abc25d33115b3a7c219cae158f4652
Aegis-ID <pierre.nguyen@epitech.eu>
2024-02-13 11:27:25 +0100

    {fix} coding style

--- lib/base_funcs/my_compute_square_root.c
+++ lib/base_funcs/my_compute_square_root.c
@@ -7,23 +7,22 @@
 
 static double my_fabs(double nb)
 {
-
-	if (nb >= 0)
-		return nb;
-	return -nb;
+    if (nb >= 0)
+        return nb;
+    return -nb;
 }
 
 double my_compute_square_root(double nb)
 {
-	double save = nb;
-	double result;
+    double save = nb;
+    double result;
 
     if (nb < 2)
-		return nb;
-	result = (save + (nb / save)) / 2;
-	while (my_fabs(nb - result) >= 0.00001) {
-		save = result;
-		result = (save + (nb / save)) / 2;
-	}
-	return result;
+        return nb;
+    result = (save + (nb / save)) / 2;
+    while (my_fabs(nb - result) >= 0.00001) {
+        save = result;
+        result = (save + (nb / save)) / 2;
+    }
+    return result;
 }
